6e221df9d31d06ccd1a5a944b58e816b82d63066
彭帅 <970781372@qq.com>
2016-01-06 00:50:35 +0800

    初始化项目

---
+++ PGImageSetter/PGImageSetter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGImageSetter
+//
+//  Created by 彭帅 on 16/1/6.
+//  Copyright © 2016年 彭帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PGImageSetter/PGImageSetter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PGImageSetter
+//
+//  Created by 彭帅 on 16/1/6.
+//  Copyright © 2016年 彭帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
